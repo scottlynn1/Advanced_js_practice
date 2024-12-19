48883984ef4a7682033563c0252747d2ad8a6d74
scottlynn1 <scottlynn234@yahoo.com>
2024-12-19 07:31:33 -0500

    add scores, match reset, and end game functionality

--- Advanced_practice_2/script.js
+++ Advanced_practice_2/script.js
@@ -1,6 +1,6 @@
 //logic for player creation
-function player (name, symbol) {
-    return {name, symbol}
+function player (name, symbol, score) {
+    return {name, symbol, score}
 }
 
 //logic for gameboard
@@ -18,10 +18,8 @@ const gameboard = (function () {
 
 //logic for flow of game
 const game = (function () {
-    let Player1 = ''
-    let Player2 = ''
 
-    let firstPlayersTurn = [true];
+    const firstPlayersTurn = [true];
 
     const changePlayer = () => {
         if (firstPlayersTurn[0] === true) {
@@ -66,11 +64,17 @@ const game = (function () {
 })();
 
 function screenController () {
-
+//buttons forms and modals
     const form = document.querySelector('form');
     const boardDisplay = document.querySelector('#boardDisplay');
+    const squares = boardDisplay.querySelectorAll('div');
     const gameStart = document.querySelector("#gameStart");
+    const matchRestart = document.querySelector('#restartMatch');
     const dialog = document.querySelector("dialog");
+    const gameState = document.querySelector('#gameState');
+    const scoreBoard = document.querySelector('#scoreBoard');
+    const scores = document.querySelectorAll('span');
+    const endGame = document.querySelector('#endGame');
 
 //form values
     const player1Name = document.querySelector('#Player_1_Name');
@@ -78,48 +82,82 @@ function screenController () {
     const player2Name = document.querySelector('#Player_2_Name');
     const player2Symbol = document.querySelector('#Player_2_Symbol');
 
-    const squares = boardDisplay.querySelectorAll('div');
-    
+
+//functions and eventListeners
     const renderBoard = function () {
         squares.forEach((square, i) => {
             square.textContent = gameboard.board[i];
         });
     };
 
+    const renderScores = function() {
+        scores[2].textContent = game.Player1.score;
+        scores[5].textContent = game.Player2.score;
+    }
+
+    const activate = function (e) {
+        let square = e.target.dataset.position;
+        if (gameboard.board[Number(square)] === null) {
+            let currentPlayer = '';
+            if (game.firstPlayersTurn[0]) {
+                currentPlayer = game.Player1;
+            } else {
+                currentPlayer = game.Player2;
+            }
+            gameboard.board[Number(square)] = currentPlayer.symbol;
+            renderBoard();
+            if (game.checkwinner()) {
+                gameState.textContent = `${currentPlayer.name} wins!`;
+                currentPlayer.score = ++currentPlayer.score;
+                renderScores();
+                restart();
+            } else if (game.checkTie()) {
+                gameState.textContent = 'Tie!';
+                restart();
+            } else {
+            game.changePlayer();
+            }
+        }
+    };
+
+    const restart = function () {
+        gameboard.clearBoard();
+        renderBoard();
+        game.firstPlayersTurn[0] = true;
+    };
+
+    const end = function () {
+        restart();
+        delete game.Player1;
+        delete game.Player2;
+        scoreBoard.setAttribute('hidden', '');
+        boardDisplay.removeEventListener('click', activate);
+        endGame.removeEventListener('click', end);
+    };
+
 //activate board function upon game start
     function activateBoard() {
-        boardDisplay.addEventListener('click', (e) => {
-            let square = e.target.dataset.position;
-            if (gameboard.board[Number(square)] === null) {
-                let currentPlayer = '';
-                if (game.firstPlayersTurn[0]) {
-                    currentPlayer = game.Player1;
-                } else {
-                    currentPlayer = game.Player2;
-                }
-                gameboard.board[Number(square)] = currentPlayer.symbol;
-                renderBoard();
-                if (game.checkwinner()) {
-                    console.log(`${currentPlayer.name} wins!`);
-                    gameboard.clearBoard();
-                    renderBoard();
-                } else if (game.checkTie()) {
-                    console.log('Tie!');
-                    gameboard.clearBoard();
-                    renderBoard();
-                }
-                game.changePlayer();
-            }
-        });
+        boardDisplay.addEventListener('click', activate);
     }
 
     gameStart.addEventListener("click", ()=>{dialog.showModal();});
 
+    matchRestart.addEventListener("click", restart);
+
+
+
+
     form.addEventListener('submit', (e)=>{
         e.preventDefault();
-        game.Player1 = player(player1Name.value, player1Symbol.value)
-        game.Player2 = player(player2Name.value, player2Symbol.value)
+        game.Player1 = player(player1Name.value, player1Symbol.value, 0);
+        game.Player2 = player(player2Name.value, player2Symbol.value, 0);
+        scoreBoard.removeAttribute('hidden');
+        scores[0].textContent = game.Player1.name;
+        scores[2].textContent = game.Player1.score;
+        scores[3].textContent = game.Player2.name;
+        scores[5].textContent = game.Player2.score;
         activateBoard();
+        endGame.addEventListener('click', end);
         form.reset();
         dialog.close();
     });
